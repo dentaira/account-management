9c042c0955e5a9a72c7acb3875e4ec0d057509a8
dentaira <heismanreport.05@gmail.com>
2024-03-17 13:06:44 +0900

    ADD DateTimeFactory

---
+++ account-management-backend/src/main/java/dentaira/accountmanagement/common/DateTimeFactory.java
@@ -0,0 +1,18 @@
+package dentaira.accountmanagement.common;
+
+import org.springframework.stereotype.Component;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Component
+public class DateTimeFactory {
+
+    private final Clock clock = Clock.systemDefaultZone();
+
+    public Instant now() {
+        return clock.instant().truncatedTo(ChronoUnit.SECONDS);
+    }
+
+}
