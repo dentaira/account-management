ed7ab62629e559ddf3160c49e20844322847873e
dentaira <heismanreport.05@gmail.com>
2024-03-23 07:00:16 +0900

    Modify ModularityTest
    ドキュメントを出力する

---
+++ account-management-backend/src/test/java/dentaira/accountmanagement/ModularityTest.java
@@ -0,0 +1,25 @@
+package dentaira.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+public class ModularityTest {
+
+    ApplicationModules modules = ApplicationModules.of(AccountManagementBackendApplication.class);
+
+    @Test
+    void verify() {
+        modules.forEach(System.out::println);
+        modules.verify();
+    }
+
+
+    @Test
+    void writeDocumentationSnippets() {
+        new Documenter(modules)
+                .writeModulesAsPlantUml()
+                .writeIndividualModulesAsPlantUml();
+    }
+
+}
