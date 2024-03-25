24cfa66f149617251fe9f48167dca7856f2751f0
dentaira <heismanreport.05@gmail.com>
2024-03-26 02:58:55 +0900

    ADD レイヤーのテスト

--- account-management-backend/src/test/java/dentaira/accountmanagement/ModularityTest.java
+++ account-management-backend/src/test/java/dentaira/accountmanagement/ModularityTest.java
@@ -1,5 +1,8 @@
 package dentaira.accountmanagement;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
@@ -9,7 +12,7 @@ public class ModularityTest {
   ApplicationModules modules = ApplicationModules.of(AccountManagementBackendApplication.class);
 
   @Test
-  void verify() {
+  void verifyModules() {
     modules.forEach(System.out::println);
     modules.verify();
   }
@@ -17,5 +20,16 @@ public class ModularityTest {
   @Test
   void writeDocumentationSnippets() {
     new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
+  }
+
+  @Test
+  void verifyLayer() {
+    JavaClasses classes = new ClassFileImporter().importPackages("dentaira.accountmanagement");
+    Architectures.onionArchitecture()
+        .applicationServices("dentaira.accountmanagement.query")
+        .adapter("web", "dentaira.accountmanagement.adapter.web")
+        .adapter("infra", "dentaira.accountmanagement..infra..")
+        .withOptionalLayers(true)
+        .check(classes);
   }
 }
