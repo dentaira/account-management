58606cb9518f31334c69728c0680e854c57c99fe
dentaira <heismanreport.05@gmail.com>
2024-03-25 23:57:19 +0900

    Refactoring コメント追加

--- account-management-backend/src/main/java/dentaira/accountmanagement/user/domain/UserRepository.java
+++ account-management-backend/src/main/java/dentaira/accountmanagement/user/domain/UserRepository.java
@@ -1,18 +1,48 @@
 package dentaira.accountmanagement.user.domain;
 
 import dentaira.accountmanagement.common.EmailAddress;
+import dentaira.accountmanagement.common.EntityUpdateConflictException;
 import dentaira.accountmanagement.user.UserId;
 import java.util.Optional;
 
 public interface UserRepository {
 
+  /**
+   * ユーザーIDを生成する
+   *
+   * @return ユーザーID
+   */
   UserId generateId();
 
+  /**
+   * ユーザーIDでユーザーを検索する
+   *
+   * @param userId ユーザーID
+   * @return ユーザー
+   */
   Optional<User> findById(UserId userId);
 
+  /**
+   * メールアドレスでユーザーを検索する
+   *
+   * @param email メールアドレス
+   * @return ユーザー
+   */
   Optional<User> findByEmail(EmailAddress email);
 
+  /**
+   * ユーザーを保存する
+   *
+   * @param user ユーザー
+   */
   void save(User user);
 
+  /**
+   * ユーザーを更新する
+   *
+   * @param user ユーザー
+   * @return 更新後のユーザー
+   * @throws EntityUpdateConflictException バージョンが一致しない場合
+   */
   User update(User user);
 }
--- account-management-backend/src/main/java/dentaira/accountmanagement/user/domain/UserService.java
+++ account-management-backend/src/main/java/dentaira/accountmanagement/user/domain/UserService.java
@@ -10,12 +10,24 @@ import dentaira.accountmanagement.user.UserStatus;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/** ユーザーを更新するドメインロジックを提供する */
 @Service
 @AllArgsConstructor
 public class UserService {
 
   private final DateTimeFactory dateTimeFactory;
 
+  /**
+   * ユーザーを作成する
+   *
+   * @param userId ユーザーID
+   * @param memberId 会員ID
+   * @param email ユーザー登録に使用可能なメールアドレス
+   * @param encryptedPassword 暗号化されたパスワード
+   * @param name 名前
+   * @param role ユーザー権限
+   * @return ユーザー
+   */
   public User create(
       UserId userId,
       MemberId memberId,
@@ -37,6 +49,15 @@ public class UserService {
         now);
   }
 
+  /**
+   * ユーザーを編集する
+   *
+   * @param user ユーザー
+   * @param name 名前
+   * @param role ユーザー権限
+   * @param activate 有効化する場合はtrue
+   * @return 編集後のユーザー
+   */
   public User edit(User user, String name, UserRole role, boolean activate) {
     var newStatus = activate ? UserStatus.Active : UserStatus.Inactive;
     if (!user.status().canTransitTo(newStatus)) {
@@ -51,6 +72,13 @@ public class UserService {
         .build();
   }
 
+  /**
+   * メールアドレスを変更する
+   *
+   * @param user ユーザー
+   * @param email メールアドレス
+   * @return 変更後のユーザー
+   */
   public User changeEmail(User user, EmailAddress email) {
     return user.toBuilder().withEmail(email).withUpdatedAt(dateTimeFactory.now()).build();
   }
